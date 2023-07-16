260b2ab9abdd151a0757e7c1137bc8d2d3a98b46
Emanuelle Marreira <emanuellemarreira@gmail.com>
2023-07-16 15:33:50 -0400

    adicionando o codigo do exercicio

---
+++ caixaeletronico.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int valor, notas_100, notas_50, notas_10, notas_5, notas_1;
+    
+    printf("Digite o valor do saque (entre 10 e 600 reais): ");
+    scanf("%d", &valor);
+    
+    if (valor < 10 || valor > 600) {
+        printf("Valor inválido. O valor mínimo é 10 reais e o máximo é 600 reais.\n");
+        return 0;
+    }
+    
+    notas_100 = valor / 100;
+    valor = valor % 100;
+    
+    notas_50 = valor / 50;
+    valor = valor % 50;
+    
+    notas_10 = valor / 10;
+    valor = valor % 10;
+    
+    notas_5 = valor / 5;
+    valor = valor % 5;
+    
+    notas_1 = valor;
+    
+    printf("Notas fornecidas:\n");
+    printf("Notas de 100 reais: %d\n", notas_100);
+    printf("Notas de 50 reais: %d\n", notas_50);
+    printf("Notas de 10 reais: %d\n", notas_10);
+    printf("Notas de 5 reais: %d\n", notas_5);
+    printf("Notas de 1 real: %d\n", notas_1);
+    
+    return 0;
+}
+
